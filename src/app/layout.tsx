ce37fc4b3e8358b7a3ba550612babcc0e2f83013
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2025-02-21 22:41:50 +0200

    test(linting): husky check2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
-  console.log(2);
+  console.log(3);
   return (
     <html lang="en">
       <body className={`${fonts.inter.className} antialiased`}>
