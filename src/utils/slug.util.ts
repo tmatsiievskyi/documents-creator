169dfc0b54f948e8546495de49319c32e9e87d0d
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2025-03-06 07:55:23 +0200

    feat(drizzle): added drizzle

--- src/utils/slug.util.ts
+++ src/utils/slug.util.ts
@@ -1,52 +1,52 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-import slugify from 'slugify';
+// import { PrismaClient, Prisma } from '@prisma/client'; //TODO: r
+// import slugify from 'slugify';
 
-type TModelNames = keyof typeof Prisma.ModelName;
-type TLowerCaseModelNames = Lowercase<TModelNames>;
+// type TModelNames = keyof typeof Prisma.ModelName;
+// type TLowerCaseModelNames = Lowercase<TModelNames>;
 
-export class SlugGenerator {
-  constructor(private readonly prismaClient: PrismaClient) {}
+// export class SlugGenerator {
+//   constructor(private readonly prismaClient: PrismaClient) {}
 
-  async generateUniqueSlug(
-    title: string,
-    modelName: TLowerCaseModelNames = 'document',
-    maxAttempts: number = 10
-  ) {
-    const baseSlug = this.createBaseSlug(title);
-    let slug = baseSlug;
-    let counter = 0;
+//   async generateUniqueSlug(
+//     title: string,
+//     modelName: TLowerCaseModelNames = 'document',
+//     maxAttempts: number = 10
+//   ) {
+//     const baseSlug = this.createBaseSlug(title);
+//     let slug = baseSlug;
+//     let counter = 0;
 
-    while ((await this.isSlugExists(slug, modelName)) && counter < maxAttempts) {
-      counter++;
-      slug = `${baseSlug}-${counter}`;
-    }
+//     while ((await this.isSlugExists(slug, modelName)) && counter < maxAttempts) {
+//       counter++;
+//       slug = `${baseSlug}-${counter}`;
+//     }
 
-    if (counter === maxAttempts) {
-      const randomSuffix = Math.random().toString(36).substring(2, 8);
-      slug = `${baseSlug}-${randomSuffix}`;
-    }
+//     if (counter === maxAttempts) {
+//       const randomSuffix = Math.random().toString(36).substring(2, 8);
+//       slug = `${baseSlug}-${randomSuffix}`;
+//     }
 
-    return slug;
-  }
+//     return slug;
+//   }
 
-  private createBaseSlug(title: string) {
-    return slugify(title, {
-      replacement: '-',
-      remove: /[*+~.()'"!:@]/g,
-      lower: true,
-      strict: true,
-      locale: 'uk', // TODO: get from i18
-      trim: true,
-    });
-  }
+//   private createBaseSlug(title: string) {
+//     return slugify(title, {
+//       replacement: '-',
+//       remove: /[*+~.()'"!:@]/g,
+//       lower: true,
+//       strict: true,
+//       locale: 'uk', // TODO: get from i18
+//       trim: true,
+//     });
+//   }
 
-  private async isSlugExists(slug: string, modelName: TLowerCaseModelNames) {
-    if (modelName === 'document') {
-      const count = await this.prismaClient[modelName].count({
-        where: { slug },
-      });
+//   private async isSlugExists(slug: string, modelName: TLowerCaseModelNames) {
+//     if (modelName === 'document') {
+//       const count = await this.prismaClient[modelName].count({
+//         where: { slug },
+//       });
 
-      return count > 0;
-    }
-  }
-}
+//       return count > 0;
+//     }
+//   }
+// }
