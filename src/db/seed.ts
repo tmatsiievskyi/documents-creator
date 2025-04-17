95accfb87dc1f85d6463e3dd2900a7c29a0327ee
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2025-04-17 11:50:49 +0300

    fix(seed): removed seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,129 +1,129 @@
-import { eq } from 'drizzle-orm';
-import { database } from './index';
-import { companiesTable, TCompaniesInsert } from './schema/companies';
-import { documentsTable, TDocumentInsert } from './schema/documents';
-import { TUser, TUserInsert, usersTable } from './schema/user';
-import { timeUTC } from '@/utils';
+// import { eq } from 'drizzle-orm';
+// import { database } from './index';
+// import { companiesTable, TCompaniesInsert } from './schema/companies';
+// import { documentsTable, TDocumentInsert } from './schema/documents';
+// import { TUser, TUserInsert, usersTable } from './schema/user';
+// import { timeUTC } from '@/utils';
 
-const companiesToCreate: TCompaniesInsert[] = [
-  {
-    name: 'Company A',
-  },
-  {
-    name: 'Company B',
-  },
-];
+// const companiesToCreate: TCompaniesInsert[] = [
+//   {
+//     name: 'Company A',
+//   },
+//   {
+//     name: 'Company B',
+//   },
+// ];
 
-const seed = async () => {
-  try {
-    await database.delete(documentsTable);
-    await database.delete(usersTable);
-    await database.delete(companiesTable);
+// const seed = async () => {
+//   try {
+//     await database.delete(documentsTable);
+//     await database.delete(usersTable);
+//     await database.delete(companiesTable);
 
-    const companyIds = await seedCompanies(companiesToCreate);
+//     const companyIds = await seedCompanies(companiesToCreate);
 
-    const createdUsers = await seedUsers(companyIds);
+//     const createdUsers = await seedUsers(companyIds);
 
-    await seedDocuments(createdUsers, companyIds);
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+//     await seedDocuments(createdUsers, companyIds);
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
-const seedCompanies = async (newCompanies: TCompaniesInsert[]) => {
-  const insertedCompanies = await database
-    .insert(companiesTable)
-    .values(newCompanies)
-    .returning({ id: companiesTable.id });
+// const seedCompanies = async (newCompanies: TCompaniesInsert[]) => {
+//   const insertedCompanies = await database
+//     .insert(companiesTable)
+//     .values(newCompanies)
+//     .returning({ id: companiesTable.id });
 
-  return insertedCompanies.map(item => item.id);
-};
+//   return insertedCompanies.map(item => item.id);
+// };
 
-const seedUsers = async (companyIds: string[]) => {
-  const baseUser: TUserInsert[] = [{ email: 'base_user@gmail.com' }];
+// const seedUsers = async (companyIds: string[]) => {
+//   const baseUser: TUserInsert[] = [{ email: 'base_user@gmail.com' }];
 
-  const usersForCompanies: TUserInsert[] = companyIds.flatMap((id, index) => {
-    return [
-      {
-        email: `user1_${index}@${id}.com`,
-        companyId: id,
-        emailVerified: timeUTC(),
-      },
-      {
-        email: `user2_${index}@${id}.com`,
-        companyId: id,
-        emailVerified: timeUTC(),
-      },
-    ];
-  });
+//   const usersForCompanies: TUserInsert[] = companyIds.flatMap((id, index) => {
+//     return [
+//       {
+//         email: `user1_${index}@${id}.com`,
+//         companyId: id,
+//         emailVerified: timeUTC(),
+//       },
+//       {
+//         email: `user2_${index}@${id}.com`,
+//         companyId: id,
+//         emailVerified: timeUTC(),
+//       },
+//     ];
+//   });
 
-  const insertedUsers = await database
-    .insert(usersTable)
-    .values([...baseUser, ...usersForCompanies])
-    .returning();
+//   const insertedUsers = await database
+//     .insert(usersTable)
+//     .values([...baseUser, ...usersForCompanies])
+//     .returning();
 
-  return insertedUsers;
-};
+//   return insertedUsers;
+// };
 
-const seedDocuments = async (inputUsers: TUser[], companyIds: string[]) => {
-  const documentsArr: TDocumentInsert[] = [];
+// const seedDocuments = async (inputUsers: TUser[], companyIds: string[]) => {
+//   const documentsArr: TDocumentInsert[] = [];
 
-  inputUsers.forEach((user, index) => {
-    for (let i = 0; i < 2; i++) {
-      documentsArr.push({
-        status: 'PUBLISHED',
-        content: JSON.stringify({
-          type: 'doc',
-          content: [
-            {
-              type: 'paragraph',
-              content: [{ type: 'text', text: `${user.email} document #${index}` }],
-            },
-          ],
-        }),
-        ownerType: 'USER',
-        ownerUserId: user.id,
-        authorId: user.id,
-      });
-    }
-  });
+//   inputUsers.forEach((user, index) => {
+//     for (let i = 0; i < 2; i++) {
+//       documentsArr.push({
+//         status: 'PUBLISHED',
+//         content: JSON.stringify({
+//           type: 'doc',
+//           content: [
+//             {
+//               type: 'paragraph',
+//               content: [{ type: 'text', text: `${user.email} document #${index}` }],
+//             },
+//           ],
+//         }),
+//         ownerType: 'USER',
+//         ownerUserId: user.id,
+//         authorId: user.id,
+//       });
+//     }
+//   });
 
-  for (const companyId of companyIds) {
-    const companyUsersIds = await database
-      .select({ id: usersTable.id })
-      .from(usersTable)
-      .where(eq(usersTable.companyId, companyId));
+//   for (const companyId of companyIds) {
+//     const companyUsersIds = await database
+//       .select({ id: usersTable.id })
+//       .from(usersTable)
+//       .where(eq(usersTable.companyId, companyId));
 
-    for (let i = 0; i < 3; i++) {
-      const authorId = companyUsersIds[Math.floor(Math.random() * companyUsersIds.length)].id;
+//     for (let i = 0; i < 3; i++) {
+//       const authorId = companyUsersIds[Math.floor(Math.random() * companyUsersIds.length)].id;
 
-      documentsArr.push({
-        status: 'PUBLISHED',
-        content: JSON.stringify({
-          type: 'doc',
-          content: [
-            {
-              type: 'paragraph',
-              content: [
-                { type: 'text', text: `Company: #${companyId}. Author: ${companyUsersIds[0].id}` },
-              ],
-            },
-          ],
-        }),
-        ownerType: 'COMPANY',
-        ownerCompanyId: companyId,
-        authorId: authorId,
-      });
-    }
-  }
+//       documentsArr.push({
+//         status: 'PUBLISHED',
+//         content: JSON.stringify({
+//           type: 'doc',
+//           content: [
+//             {
+//               type: 'paragraph',
+//               content: [
+//                 { type: 'text', text: `Company: #${companyId}. Author: ${companyUsersIds[0].id}` },
+//               ],
+//             },
+//           ],
+//         }),
+//         ownerType: 'COMPANY',
+//         ownerCompanyId: companyId,
+//         authorId: authorId,
+//       });
+//     }
+//   }
 
-  const insertedDocuments = await database.insert(documentsTable).values(documentsArr).returning();
-  console.dir({ insertedDocuments }, { deepth: 3 });
-};
+//   const insertedDocuments = await database.insert(documentsTable).values(documentsArr).returning();
+//   console.dir({ insertedDocuments }, { deepth: 3 });
+// };
 
-seed()
-  .catch(console.error)
-  .finally(() => {
-    process.exit(0);
-  });
+// seed()
+//   .catch(console.error)
+//   .finally(() => {
+//     process.exit(0);
+//   });
